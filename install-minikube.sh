a7ba8f8b7e064b7c3664927eb665d98f77cc1de7
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-26 17:46:17 -0300

    26012025-1746

--- install-minikube.sh
+++ install-minikube.sh
@@ -50,7 +50,7 @@ ___console_logs 'Configure Kickoff Minikube Cluster on Machine Startup'
 echo $SUDO_PASS | sudo -S touch /etc/systemd/system/minikube.service
 
 ___console_logs 'ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'
-echo toor | sudo -S systemctl cat <<EOF > /etc/systemd/system/minikube.service
+echo toor | sudo -S cat <<EOF > /etc/systemd/system/minikube.service
 [Unit]
 Description=Kickoff Minikube Cluster
 After=docker.service
