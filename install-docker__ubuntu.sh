0e2b5a238709a54bfbc5c23b1357642c09139633
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-26 16:32:07 -0300

    26012025-1632

--- install-docker__ubuntu.sh
+++ install-docker__ubuntu.sh
@@ -19,7 +19,7 @@ ___console_logs '[02/11] Install a few prerequisite packages'
 echo $SUDO_PASS | sudo -S apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 ___console_logs '[03/11] Add the GPG key for the official Docker repository'
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | echo $SUDO_PASS | sudo -S gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 ___console_logs '[04/11] Add the Docker repository to APT sources'
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | echo $SUDO_PASS | sudo -S tee /etc/apt/sources.list.d/docker.list > /dev/null
