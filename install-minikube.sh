196eb60c850ed53b47ebd6bb2987832dfb66193a
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-26 18:45:28 -0300

    26012025-1845

--- install-minikube.sh
+++ install-minikube.sh
@@ -75,11 +75,9 @@ cp ~/.minikube/profiles/minikube/client.crt nginx/minikube
 cp ~/.minikube/profiles/minikube/client.key nginx/minikube
 cp ~/.minikube/ca.crt nginx/minikube
 
-___console_logs '[09/09] Copy the certificate and key'
+___console_logs '[09/09] Create NGINX password'
 echo $SUDO_PASS | sudo -S apt install -y apache2-utils
-
-___console_logs '[09/09] Create a ngix password'
-htpasswd -c nginx/minikube/.htpasswd $SO_USER
+echo $SUDO_PASS | sudo -S htpasswd -c nginx/minikube/.htpasswd $SO_USER
 
 
 
