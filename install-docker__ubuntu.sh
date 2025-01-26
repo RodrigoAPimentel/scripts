eba9a3b8647f29c89eb7c5f13ce32c906639d40e
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-26 16:01:11 -0300

    26012025-1601

--- install-docker__ubuntu.sh
+++ install-docker__ubuntu.sh
@@ -47,42 +47,42 @@ export LANGUAGE=en_US.UTF-8
 export LC_COLLATE=C
 export LC_CTYPE=en_US.UTF-8
 
-___console_logs 'Update and Upgrade System'
+___console_logs '[01/11] Update and Upgrade System'
 sudo apt update && sudo apt upgrade -y
 
-___console_logs 'Install a few prerequisite packages'
+___console_logs '[02/11] Install a few prerequisite packages'
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-___console_logs 'Add the GPG key for the official Docker repository'
+___console_logs '[03/11] Add the GPG key for the official Docker repository'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-___console_logs 'Add the Docker repository to APT sources'
+___console_logs '[04/11] Add the Docker repository to APT sources'
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-___console_logs 'Update your existing list of packages again for the addition to be recognized'
+___console_logs '[05/11] Update your existing list of packages again for the addition to be recognized'
 sudo apt update
 
-___console_logs 'Make sure you are about to install from the Docker repo instead of the default Ubuntu repo'
+___console_logs '[06/11] Make sure you are about to install from the Docker repo instead of the default Ubuntu repo'
 apt-cache policy docker-ce
 
-___console_logs 'Install Docker'
+___console_logs '[07/11] Install Docker'
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-___console_logs 'Docker Status'
+___console_logs '[08/11] Docker Status'
 sudo systemctl status docker
 
 # ___console_logs 'Activating and starting the docker service'
 # systemctl start docker
 # systemctl enable --now docker.service
 
-___console_logs 'Installing Docker Compose'
-curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+___console_logs '[09/11] Installing Docker Compose'
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-___console_logs 'Add your username to the docker group'
+___console_logs '[10/11] Add your username to the docker group'
 sudo usermod -aG docker ${USER} && su - ${USER}
 
-___console_logs 'Restarting the machine'
+___console_logs '[11/11] Restarting the machine'
 reboot --force
 
 echo " " 
