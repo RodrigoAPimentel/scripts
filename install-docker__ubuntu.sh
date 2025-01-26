e1577fe0e88ddad200c37204146f2ed217138dd6
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-26 16:18:01 -0300

    26012025-1617

--- install-docker__ubuntu.sh
+++ install-docker__ubuntu.sh
@@ -1,34 +1,3 @@
-# ___console_logs () {
-#     echo " "
-#     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-#     echo ">>>>>>>>> $1 ..."
-#     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-#     sleep 2
-# }
-
-# echo '##########################################################################'
-# echo '############ INSTALL DOCKER AND DOCKER-COMPOSE [CentOS 8 - Stream] #######'
-# echo '##########################################################################\n'
-
-
-# ___console_logs 'Installing required packages'
-# dnf install -y dnf-plugins-core
-
-# ___console_logs 'Configuring the Docker repository'
-# dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-
-# ___console_logs 'Installing Docker Engine'
-# dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-
-# echo " "
-# echo '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-# echo '<<<<<<<<<<<<<< END <<<<<<<<<<<<<<<<<<<<<<<<<'
-# echo '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-
-
-
-
 ___console_logs () {
     echo " "
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
