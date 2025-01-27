b691b6a1478e69b2a360b9839cba2fe9275c3755
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-26 23:45:31 -0300

    26012025-2345

--- 2025/install-docker__ubuntu.sh
+++ 2025/install-docker__ubuntu.sh
@@ -12,40 +12,37 @@ echo '##########################################################################
 echo '############ INSTALL DOCKER AND DOCKER-COMPOSE [Ubuntu 22.04] ############'
 echo '##########################################################################\n'
 
-___console_logs '[01/11] Update and Upgrade System'
+___console_logs '[01/10] Update and Upgrade System'
 echo $SUDO_PASS | sudo -S apt update && echo $SUDO_PASS | sudo -S apt upgrade -y
 
-___console_logs '[02/11] Install a few prerequisite packages'
+___console_logs '[02/10] Install a few prerequisite packages'
 echo $SUDO_PASS | sudo -S apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-___console_logs '[03/11] Add the GPG key for the official Docker repository'
+___console_logs '[03/10] Add the GPG key for the official Docker repository'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-___console_logs '[04/11] Add the Docker repository to APT sources'
+___console_logs '[04/10] Add the Docker repository to APT sources'
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-___console_logs '[05/11] Update your existing list of packages again for the addition to be recognized'
+___console_logs '[05/10] Update your existing list of packages again for the addition to be recognized'
 echo $SUDO_PASS | sudo -S apt update
 
-___console_logs '[06/11] Make sure you are about to install from the Docker repo instead of the default Ubuntu repo'
+___console_logs '[06/10] Make sure you are about to install from the Docker repo instead of the default Ubuntu repo'
 apt-cache policy docker-ce
 
-___console_logs '[07/11] Install Docker'
+___console_logs '[07/10] Install Docker'
 echo $SUDO_PASS | sudo -S apt install -y docker-ce
 
-___console_logs '[08/11] Docker Status'
-echo $SUDO_PASS | sudo -S systemctl status docker
-
-___console_logs '[09/11] Install Docker Compose'
+___console_logs '[08/10] Install Docker Compose'
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 docker compose version
 
-___console_logs '[10/11] Add your username to the docker group'
+___console_logs '[09/10] Add your username to the docker group'
 echo $SUDO_PASS | sudo -S usermod -aG docker ${USER}
 
-___console_logs '[11/11] Restarting the machine'
+___console_logs '[10/10] Restarting the machine'
 echo $SUDO_PASS | sudo -S reboot --force
 
 echo " " 
--- 2025/install-oh_my_zsh__ubuntu.sh
+++ 2025/install-oh_my_zsh__ubuntu.sh
@@ -15,61 +15,62 @@ echo '##########################################################################
 echo '############################ INSTALL OH-MY-ZSH ###########################'
 echo '##########################################################################\n'
 
-___console_logs '[01/11] Install a few prerequisite packages'
-echo $SUDO_PASS | sudo -S apt install -y zsh golang-go fontconfig
+# ___console_logs '[01/11] Install a few prerequisite packages'
+# echo $SUDO_PASS | sudo -S apt install -y zsh golang-go fontconfig
 
-___console_logs '[02/11] Download oh-my-zsh'
-echo $SUDO_PASS | sudo -S rm -r $HOME/.oh-my-zsh
+# ___console_logs '[02/11] Download oh-my-zsh'
+# echo $SUDO_PASS | sudo -S rm -r $HOME/.oh-my-zsh
 
-echo $SUDO_PASS | sudo -S rm -Rf /root/.oh-my-zsh
-echo $SUDO_PASS | sudo -S rm -Rf ~/.zshrc
-echo $SUDO_PASS | sudo -S chsh -s /bin/zsh root
-echo $SUDO_PASS | sudo -S echo $SHELL
+# echo $SUDO_PASS | sudo -S rm -Rf /root/.oh-my-zsh
+# echo $SUDO_PASS | sudo -S rm -Rf ~/.zshrc
+# echo $SUDO_PASS | sudo -S chsh -s /bin/zsh root
+# echo $SUDO_PASS | sudo -S echo $SHELL
 
-___console_logs '[03/11] Install oh-my-zsh'
-echo $SUDO_PASS | sudo -S wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-echo $SUDO_PASS | sudo -S /bin/cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+# ___console_logs '[03/11] Install oh-my-zsh'
+# echo $SUDO_PASS | sudo -S wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+# echo $SUDO_PASS | sudo -S /bin/cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
-___console_logs '[04/11] Downloading, Install and configuring plugins plugins'
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions && autoload -U compinit && compinit
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && sed -i '/ask ".*/s/^/#/g' ~/.fzf/install && ~/.fzf/install
-sed -i "s|^plugins=(git)|plugins=(git zsh-syntax-highlighting fzf zsh-autosuggestions k zsh-completions)|g" ~/.zshrc
+# ___console_logs '[04/11] Downloading, Install and configuring plugins plugins'
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions && autoload -U compinit && compinit
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && sed -i '/ask ".*/s/^/#/g' ~/.fzf/install && ~/.fzf/install
+# sed -i "s|^plugins=(git)|plugins=(git zsh-syntax-highlighting fzf zsh-autosuggestions k zsh-completions)|g" ~/.zshrc
 
-___console_logs '[05/11] Downloading and Install Powerline Fonts'
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
+# ___console_logs '[05/11] Downloading and Install Powerline Fonts'
+# git clone https://github.com/powerline/fonts.git --depth=1
+# cd fonts
+# ./install.sh
+# cd ..
+# rm -rf fonts
 
-___console_logs '[06/11] Download and Install PowerLevel10K Theme'
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# ___console_logs '[06/11] Download and Install PowerLevel10K Theme'
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-___console_logs '[07/11] Configuring Theme'
-sed -i "s|^ZSH_THEME=.*|ZSH_THEME=$_ZSH_THEME|g" ~/.zshrc
+# ___console_logs '[07/11] Configuring Theme [$_ZSH_THEME]'
+# sed -i "s|^ZSH_THEME=.*|ZSH_THEME=$_ZSH_THEME|g" ~/.zshrc
 
-___console_logs '[08/11] Copy Powerlevel10k configuration file'
-rm -rfv ~/.p10k.zsh 
-cp -rv 2025/resources/p10k_zsh_plugin_configuration.txt ~/.p10k.zsh
+# ___console_logs '[08/11] Copy Powerlevel10k configuration file'
+# rm -rfv ~/.p10k.zsh 
+# cp -rv 2025/resources/p10k_zsh_plugin_configuration.txt ~/.p10k.zsh
 
-chmod 0644 ~/.p10k.zsh 
+# chmod 0644 ~/.p10k.zsh 
 
-echo $SUDO_PASS | sudo -S chown root ~/.p10k.zsh 
-echo $SUDO_PASS | sudo -S chgrp root ~/.p10k.zsh 
+# echo $SUDO_PASS | sudo -S chown root ~/.p10k.zsh 
+# echo $SUDO_PASS | sudo -S chgrp root ~/.p10k.zsh 
 
-___console_logs '[09/11] Configuring Powerlevel10k in .zshrc'
-echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc 
+# ___console_logs '[09/11] Configuring Powerlevel10k in .zshrc'
+# echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc 
 
 
 ___console_logs '[10/11] Reload Terminal'
-echo $SUDO_PASS | sudo -S chsh -s $(which zsh)
+# echo $SUDO_PASS | sudo -S chsh -s $(which zsh)
+chsh -s $(which zsh)
 zsh --login
 
-___console_logs '[11/11] Restarting the machine'
-echo $SUDO_PASS | sudo -S reboot --force
+# ___console_logs '[11/11] Restarting the machine'
+# echo $SUDO_PASS | sudo -S reboot --force
 
 echo " " 
 echo '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
