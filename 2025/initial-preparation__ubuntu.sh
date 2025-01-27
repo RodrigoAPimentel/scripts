686ba2fb7bd06cc8048d7d4fdffc006bc586204d
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-26 23:25:51 -0300

    26012025-2325

--- 2025/initial-preparation__ubuntu.sh
+++ 2025/initial-preparation__ubuntu.sh
@@ -18,8 +18,9 @@ echo $SUDO_PASS | sudo -S sudo apt update
 ___console_logs '[02/04] Upgrade'
 echo $SUDO_PASS | sudo -S sudo apt update -y
 
-___console_logs '[03/04] Change time-xone settings'
+___console_logs '[03/04] Change time-zone settings'
 echo $SUDO_PASS | sudo -S sudo timedatectl set-timezone America/Recife
+date
 
 ___console_logs '[04/04] Restarting the machine'
 echo $SUDO_PASS | sudo -S reboot --force
