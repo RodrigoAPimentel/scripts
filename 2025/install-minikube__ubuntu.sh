3fc2fab1c37fe11f1d4062474da757a9051b1e01
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-28 16:56:21 -0300

    28012025-1656

--- 2025/install-minikube__ubuntu.sh
+++ 2025/install-minikube__ubuntu.sh
@@ -34,7 +34,7 @@ ___console_logs () {
 # fi
 
 # ___console_logs '[01/20] Install a few prerequisite packages'
-# echo $SUDO_PASS | sudo -S apt install -y yq
+# echo $SUDO_PASS | sudo -S apt install -yqqq tree yq
 
 # ___console_logs '[02/20] Verify Docker installed'
 # IS_DOCKER=$(which docker)
@@ -135,12 +135,13 @@ EOF
 cat $NGINX_FOLDER/nginx.conf
 
 ___console_logs '[15/20] Create Dockerfile'
+pwd
 cat <<EOF > $NGINX_FOLDER/Dockerfile
 # Official Nginx image
 FROM nginx:latest
 
 # Copy Nginx configuration file to the container
-COPY nginx.conf /etc/nginx/nginx.conf
+COPY $NGINX_FOLDER/nginx.conf /etc/nginx/nginx.conf
 
 # Copy minikube certs and password
 COPY $MINIKUBE_FOLDER/client.key /etc/nginx/certs/minikube-client.key
@@ -153,20 +154,16 @@ EXPOSE 443
 EOF
 cat $NGINX_FOLDER/Dockerfile
 
-___console_logs '[16/20] Show all Configuration Files'
-echo $SUDO_PASS | sudo -S apt install -yqqq tree
-tree -a $MINIKUBE_FOLDER
-
-___console_logs '[17/20] Build NGINX docker image'
+___console_logs '[16/20] Build NGINX docker image'
 docker build -t nginx-minikube-proxy $NGINX_FOLDER
 
-___console_logs '[18/20] Run NGINX docker image'
+___console_logs '[17/20] Run NGINX docker image'
 CONTAINER_ID=$(docker run -d --memory="500m" --memory-reservation="256m" --cpus="0.25" --restart=always --name nginx-minikube-proxy -p 443:443 -p 80:80 --network=minikube nginx-minikube-proxy)
 echo "==> CONTAINER ID: [$CONTAINER_ID]"
 echo "==> Container NGINX logs:"
 docker logs $CONTAINER_ID
 
-___console_logs '[19/20] Configure Kubeconfig to external access'
+___console_logs '[18/20] Configure Kubeconfig to external access'
 cp -rv $HOME/.kube/config $MINIKUBE_FOLDER/kubeconfig
 
 yq -yi ".clusters[0].cluster.server = \"$SO_USER:$SUDO_PASS@$IP:443\"" $MINIKUBE_FOLDER/kubeconfig 
@@ -180,6 +177,9 @@ cat $MINIKUBE_FOLDER/kubeconfig
 echo "-----\n"
 
 echo "=> See the Kubeconfig for external access to minikube at: $MINIKUBE_FOLDER/Kubeconfig"
+
+___console_logs '[19/20] Show all Configuration Files'
+tree -a $MINIKUBE_FOLDER
 
 ___console_logs '[20/20] Informations'
 echo "==> Copiar os arquivos de conexão externa gerados pela instalação do minikube: sshpass -p '$SUDO_PASS' scp -o StrictHostKeyChecking=no -r $SO_USER@$IP:$MINIKUBE_FOLDER/ <TARGET FOLDER>"
