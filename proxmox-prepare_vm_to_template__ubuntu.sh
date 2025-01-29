25ff808b192d2e0fa80c30b5b620f48ae7450310
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-28 21:19:33 -0300

    28012025-2119

--- proxmox-prepare_vm_to_template__ubuntu.sh
+++ proxmox-prepare_vm_to_template__ubuntu.sh
@@ -21,40 +21,43 @@ else
     echo "==> sudo password entered."
 fi
 
-___console_logs '[01/12] Truncate machine-id'
+___console_logs '[01/13] Update and Upgrade System'
+echo $SUDO_PASS | sudo -S apt update && echo $SUDO_PASS | sudo -S apt upgrade -y
+
+___console_logs '[02/13] Truncate machine-id'
 echo $SUDO_PASS | sudo -S truncate -s 0 /etc/machine-id
 
-___console_logs '[02/12] Install cloud-init and qemu-guest-agent'
+___console_logs '[03/13] Install cloud-init and qemu-guest-agent'
 echo $SUDO_PASS | sudo -S apt install -y cloud-init qemu-guest-agent
 
-___console_logs '[03/12] Enable qemu-guest-agent'
+___console_logs '[04/13] Enable qemu-guest-agent'
 echo $SUDO_PASS | sudo -S systemctl enable qemu-guest-agent
 
-___console_logs '[04/12] Delete ssh key host'
+___console_logs '[05/13] Delete ssh key host'
 echo $SUDO_PASS | sudo -S rm -v /etc/ssh/ssh_host_*
 
-___console_logs '[05/12] Purgue openssh-client'
+___console_logs '[06/13] Purgue openssh-client'
 echo $SUDO_PASS | sudo -S apt purge openssh-client -y
 
-___console_logs '[06/12] Apt Clean'
+___console_logs '[07/13] Apt Clean'
 echo $SUDO_PASS | sudo -S apt clean
 
-___console_logs '[07/12] Cloud-init clean'
+___console_logs '[08/13] Cloud-init clean'
 echo $SUDO_PASS | sudo -S cloud-init clean
 
-___console_logs '[08/12] Apt autoremove'
+___console_logs '[09/13] Apt autoremove'
 echo $SUDO_PASS | sudo -S apt autoremove -y
 
-___console_logs '[09/12] Copy install-openssh-server__ubuntu.sh to ~/'
+___console_logs '[10/13] Copy install-openssh-server__ubuntu.sh to ~/'
 cp -v ~/scripts/2025/install-openssh-server__ubuntu.sh ~/install_openssh-server
 
-___console_logs '[10/12] Delete scripts folder'
+___console_logs '[11/13] Delete scripts folder'
 rm -rvf ~/scripts
 
-___console_logs '[11/12] Add Cloudinit Drive Hardware'
+___console_logs '[12/13] Add Cloudinit Drive Hardware'
 echo "==> Add Cloudinit Drive Hardware. Here is an example: https://tcude.net/creating-a-vm-template-in-proxmox/"
 
-___console_logs '[12/12] Shutdown the machine'
+___console_logs '[13/13] Shutdown the machine'
 echo $SUDO_PASS | sudo -S shutdown -h now
 
 echo " " 
