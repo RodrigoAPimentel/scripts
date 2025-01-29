c7f46ad1cc96c1336d99b2a55615f4c6244e76e3
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-01-29 14:17:55 -0300

    29012025-1417

--- install-minikube__ubuntu.sh
+++ install-minikube__ubuntu.sh
@@ -149,7 +149,7 @@ server {
     location /asdf/ {
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
-        proxy_pass http://127.0.0.1:3030/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/;
+        proxy_pass http://minikube:3030/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/;
     }
 
     # location ~ ^/stash {
