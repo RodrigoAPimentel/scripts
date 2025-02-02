85c2e4ad2d0d51e2f7c555b7102726494c9e32b4
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-02-02 19:26:15 -0300

    02022025-1926

--- install-argocd.sh
+++ install-argocd.sh
@@ -2,17 +2,6 @@
 
 # OPERATION SYSTEM 
 SUDO_PASS=$1
-OS_USER=$(echo ${USER})
-OS_USER_GROUP=docker
-IP=$(hostname -I |  awk '{print $1}')
-# MINIKUBE - PATHS
-MINIKUBE_INSTALL_ROOT_FOLDER=$HOME/minikube-install
-MINIKUBE_FOLDER=$MINIKUBE_INSTALL_ROOT_FOLDER/minikube
-NGINX_FOLDER=$MINIKUBE_INSTALL_ROOT_FOLDER/nginx
-# MINIKUBE - CONFIGURATION
-KUBERNETES_DASHBOARD_DOMAIN=k8s-minikube-dashboard
-KUBERNETES_DASHBOARD_PORT=88
-MINIKUBE_ADDONS=ingress,ingress-dns,dashboard
 
 # LOADING LOG FUNCTIONS FILE
 . ./_logs.sh
@@ -28,7 +17,7 @@ else
     _log__step_result_success "==> sudo password entered."
 fi
 
-_log__step '[01/20] Verify Minikube installed'
+_log__step '[01/06] Verify Minikube installed'
 IS_MINIKUBE=$(which minikube)
 if [ -z "${IS_MINIKUBE}" ]; then
     _log__step_result_failed "Minikube NOT installed. Minikube is a basic requirement!!"
@@ -37,28 +26,33 @@ else
     _log__step_result_success "==> Minikube INSTALLED."
 fi
 
-_log__step '[02/20] Create argocd namespace'
+_log__step '[02/06] Create argocd namespace'
 kubectl create namespace argocd
 
-_log__step '[03/20] Install ArgoCD'
+_log__step '[03/06] Install ArgoCD'
 # kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.5.8/manifests/install.yaml
 echo "----------"
 _log__step_result_success "$(kubectl get pods -n argocd)"
 
-_log__step '[04/20] Download and Install argocd-cli'
+_log__step '[04/06] Download and Install argocd-cli'
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 echo "----------"
 echo $SUDO_PASS | sudo -S install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 echo "----------"
-rm -v argocd-linux-amd64
+_log__step_result_success "$(rm -v argocd-linux-amd64)"
 
 
 
-# _log__step '[04/20] Configure Access The Argo CD API Server'
+
+_log__step '[05/06] Configure Access The Argo CD API Server and GUI'
 # kubectl port-forward svc/argocd-server -n argocd --address 0.0.0.0 8080:443 ######################################################################
 
-_log__step '[20/20] Informations'
+
+
+
+
+_log__step '[06/06] Informations'
 ARGOCD_INITIAL_PASS=$(argocd admin initial-password -n argocd)
 _log__step_result_success "=====> ArgoCD Initial Password: $ARGOCD_INITIAL_PASS"
 
