a14825eba559837887b96b13725d3ea09b5d2aa2
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-02-03 19:13:37 -0300

    03022025-1913

--- install-argocd.sh
+++ install-argocd.sh
@@ -4,8 +4,8 @@
 SUDO_PASS=$1
 IP=$(hostname -I |  awk '{print $1}')
 ##### ARGOCD CONFIGURATIONS
-ARGOCD_VERSION=stable
-# ARGOCD_VERSION=v2.5.8
+# ARGOCD_VERSION=stable
+ARGOCD_VERSION=v2.5.8
 ARGOCD_DASHBOARD_DOMAIN=argocd-gui
 ARGOCD_DASHBOARD_PORT=88
 
@@ -13,7 +13,9 @@ ARGOCD_DASHBOARD_PORT=88
 . ./_logs.sh
 
 _script_start "INSTALL ARGOCD"
-##########
+
+
+
 _step '[--] Check if the sudo password was entered'
 if [ -z "${SUDO_PASS}" ]; then
     _step_result_failed "sudo password not entered!!"
@@ -22,7 +24,8 @@ if [ -z "${SUDO_PASS}" ]; then
 else
     _step_result_success "==> sudo password entered."
 fi
-##########
+
+
 _step '[01/08] Verify Minikube installed'
 IS_MINIKUBE=$(which minikube)
 if [ -z "${IS_MINIKUBE}" ]; then
@@ -31,23 +34,28 @@ if [ -z "${IS_MINIKUBE}" ]; then
 else
     _step_result_success "==> Minikube INSTALLED."
 fi
-##########
+
+
 _step '[02/08] Create argocd namespace'
 kubectl create namespace argocd
-##########
+
+
 _step '[03/08] Install ArgoCD'
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/$ARGOCD_VERSION/manifests/install.yaml
 echo "----------"
-_step_result_suggestion "> Wait pod argocd-server is Running ....."
+DD=$(date +"%H:%M:%S")
+_step_result_suggestion "> [$DD] Wait pod argocd-server is Running ....."
 kubectl -n argocd wait --for=jsonpath='{.status.phase}'=Running pod -l app.kubernetes.io/name=argocd-server --timeout=10m
 echo "----------"
 _step_result_success "$(kubectl get all -n argocd)"
-##########
+
+
 _step '[04/08] Download and Install argocd-cli'
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 echo $SUDO_PASS | sudo -S install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 _step_result_success "$(rm -v argocd-linux-amd64)"
-##########
+
+
 _step '[05/08] Configure iptable'
 RUNNING_MINIKUBE_IP=$(minikube ip)
 echo $SUDO_PASS | sudo -S iptables -t nat -A PREROUTING -p tcp --dport $ARGOCD_DASHBOARD_PORT -j DNAT --to-destination $RUNNING_MINIKUBE_IP:80
@@ -57,7 +65,8 @@ echo $SUDO_PASS | sudo -S sh -c 'ip6tables-save > /etc/iptables/rules.v6'
 _step_result_success "$(cat /etc/iptables/rules.v4 | grep -E "PREROUTING.*$ARGOCD_DASHBOARD_PORT")"
 echo "----------"
 _step_result_success "$(cat /etc/iptables/rules.v4 | grep -E "FORWARD.*$RUNNING_MINIKUBE_IP")"
-##########
+
+
 _step '[06/08] Create ArgoCD Ingress'
 cat <<EOF > ingress-argocd-dashboard.yaml
 apiVersion: networking.k8s.io/v1
@@ -89,14 +98,16 @@ echo "----------"
 _step_result_success "$(kubectl get ingress -n argocd)"
 echo "----------"
 _step_result_success "$(rm -v ingress-argocd-dashboard.yaml)"
-##########
+
+
 _step '[07/08] Disable TLS and enable SSL-PASSTHROUGH to access Dashboard externally'
 kubectl -n argocd patch deployment argocd-server --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]'
 _step_result_success "$(kubectl -n argocd describe deployment argocd-server | grep -A 3 Command)"
 echo "----------"
 kubectl -n ingress-nginx patch deployment ingress-nginx-controller --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--enable-ssl-passthrough"}]'
 _step_result_success "$(kubectl -n ingress-nginx describe deployment ingress-nginx-controller | grep -A 12 Args)"
-##########
+
+
 _step '[08/08] Informations'
 ARGOCD_INITIAL_PASS=$(argocd admin initial-password -n argocd)
 _step_result_success "=====> Usuário e senha para logar no ArgoCD Dashboard: admin|$ARGOCD_INITIAL_PASS"
@@ -108,5 +119,6 @@ _step_result_success """
           2. No navegador:
                 http://$ARGOCD_DASHBOARD_DOMAIN:$ARGOCD_DASHBOARD_PORT 
 """
-##########
+
+
 _finish_information
