818b49137a76a17817b4c63d9e80922563872459
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-03-25 17:50:28 -0300

    25032025-1750

--- applications/nodejs.sh
+++ applications/nodejs.sh
@@ -13,7 +13,8 @@ else
     echo "                 ⚠️ Node.js not found. Installing via NVM ..."
     curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
-    source "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
     nvm install --lts
     nvm use --lts
 fi
