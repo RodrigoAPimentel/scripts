cfc5ef454febed3ef88b39ebf1093b63b685d9f2
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2025-03-25 14:04:03 -0300

    25032025-1404

--- _basic_applications.sh
+++ _basic_applications.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-BASIC_PACKAGES="g++ curl tree apache2-utils golang-go cloud-init qemu-guest-agent yq gcc-c++ make python3.9 nodejs npm"
+BASIC_PACKAGES="
+    g++
+    curl
+    tree
+    apache2-utils
+    golang-go
+    cloud-init
+    qemu-guest-agent
+    yq
+    gcc-c++
+    make
+    python3.9
+    nodejs
+    npm
+"
